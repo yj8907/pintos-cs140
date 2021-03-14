0ada2c77d1b0f0d176c684076527f0d0f26ae444
Yang Jiang <yjiang@stackline.com>
2021-03-13 22:19:27 -0800

    priority donation

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -111,7 +111,7 @@ struct thread
    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
-static list_less_func priority_less;
+list_less_func priority_less;
 
 void thread_init (void);
 void thread_start (void);
