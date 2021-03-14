03b7c3a4b6997ec77e429784ac814b5e17c5a8ec
Yang Jiang <yjiang@stackline.com>
2021-03-14 01:15:41 -0800

    priority donation

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,8 +68,8 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-        list_push_back (&sema->waiters, &thread_current ()->elem);
-//      list_insert_ordered(&sema->waiters, &thread_current ()->elem, &priority_less, NULL);
+//        list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered(&sema->waiters, &thread_current ()->elem, &priority_less, NULL);
       thread_block ();
     }
   sema->value--;
