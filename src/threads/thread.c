380eca76c72ca8476c67cbfda66c7484f1cd8d15
Yang Jiang <yjiang@stackline.com>
2021-03-14 14:39:49 -0700

    priority donation

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -351,6 +351,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  if (new_priority < list_front(&ready_list)->priority) thread_yield();
 }
 
 /* Returns the current thread's priority. */
