df469582cf62bc132dd54b2a8d39bb6b341dd262
Yang Jiang <yjiang@stackline.com>
2021-03-14 01:01:03 -0800

    priority donation

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -246,8 +246,8 @@ thread_unblock (struct thread *t)
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
 
-//  list_insert_ordered(&ready_list, &t->elem, &priority_less, NULL);
-    list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list, &t->elem, &priority_less, NULL);
+//    list_push_back (&ready_list, &t->elem);
   
   t->status = THREAD_READY;
   intr_set_level (old_level);
@@ -319,8 +319,8 @@ thread_yield (void)
 
   old_level = intr_disable ();
     if (cur != idle_thread) {
-//        list_insert_ordered(&ready_list, &cur->elem, &priority_less, NULL);
-        list_push_back (&ready_list, &cur->elem);
+        list_insert_ordered(&ready_list, &cur->elem, &priority_less, NULL);
+//        list_push_back (&ready_list, &cur->elem);
     }
     
   cur->status = THREAD_READY;
