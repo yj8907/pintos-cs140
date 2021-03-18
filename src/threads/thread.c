73838a7f5bb0b59824a9b8f420501d1ef52a9fba
Yang Jiang <yjiang@stackline.com>
2021-03-17 23:09:11 -0700

    mlfqs

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -440,7 +440,7 @@ thread_exit (void)
      when it calls thread_schedule_tail(). */
   intr_disable ();
   list_remove (&thread_current()->allelem);
-  list_remove (&thread_current()->lastrun_elem);
+//  list_remove (&thread_current()->lastrun_elem);
   thread_current ()->status = THREAD_DYING;
   schedule ();
   NOT_REACHED ();
