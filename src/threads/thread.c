538f9962290b6dd9e6adb7c4ef5f5ef26fe55653
Yang Jiang <yjiang@stackline.com>
2021-03-18 21:52:55 -0700

    enable load_avg arith

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -262,7 +262,7 @@ update_mlfqs_parameters(void)
     }
     
     real f1, f2;
-    f1.val = inttoreal(59); f2.val =inttoreal(60);
+    f1.val = inttoreal(3); f2.val =inttoreal(60);
     f1.val = divide(&f1, &f2);
     
 //    load_avg.val = multiply(&f1, &load_avg);
