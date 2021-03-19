642322eca290a1a65bb81d30e511117c9a2653be
Yang Jiang <yjiang@stackline.com>
2021-03-18 17:11:01 -0700

    troubleshoot bad thread

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -412,6 +412,7 @@ thread_current (void)
      of stack, so a few big automatic arrays or moderate
      recursion can cause stack overflow. */
     if (!is_thread (t)) {
+        msg("thread is: %s", t->name);
         debug_backtrace();
     }
     
