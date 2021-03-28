1c0e44a696f794aad9f1213c196465d34336a811
Yang Jiang <yjiang@stackline.com>
2021-03-27 19:48:57 -0700

    roll back thread

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -128,10 +128,9 @@ sema_up (struct semaphore *sema)
                                     struct thread, elem));
     }
   sema->value++;
-  thread_yield();
-    
   intr_set_level (old_level);
     
+  thread_yield();
 }
 
 static void sema_test_helper (void *sema_);
