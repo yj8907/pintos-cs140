0b96f4b4c02b1132b2a05e244107028458f01e22
Yang Jiang <yjiang@stackline.com>
2021-03-27 18:52:42 -0700

    roll back thread

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -92,10 +92,8 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-   thread_sleep(start, ticks);
-//  while (timer_elapsed (start) < ticks)
-//    thread_yield ();
-
+  while (timer_elapsed (start) < ticks) 
+    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
