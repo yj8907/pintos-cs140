61b06ca3edb28d3ad6dbfe7e3423cd49f46083dc
Yang Jiang <yjiang@stackline.com>
2021-03-28 14:47:16 -0700

    arg parse

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -121,7 +121,9 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-    while(1);
+    while(true) {
+        
+    };
 }
 
 /* Free the current process's resources. */
