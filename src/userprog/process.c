97ade880b154882075fa1189befbbaf0b71f00a7
Yang Jiang <yjiang@stackline.com>
2021-03-28 22:10:47 -0700

    arg parse

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -109,8 +109,10 @@ start_process (void *file_name_)
     esp -= sizeof(esp);
     memset(esp, 0, sizeof(esp));
     
+    printf("arg count %d", *(int*)(esp+4));
+    printf("arg1 %s", *(char*)(*(esp+8)) );
     if_.esp = (void*) esp;
-    
+        
   /* If load failed, quit. */
   palloc_free_page (file_name);
   if (!success) 
