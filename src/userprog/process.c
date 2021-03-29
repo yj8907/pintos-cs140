399fad9b81cba57f6081bd4c607b7bbdde0cd999
Yang Jiang <yjiang@stackline.com>
2021-03-29 00:07:16 -0700

    arg parse

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -114,8 +114,8 @@ start_process (void *file_name_)
 
     printf("argv 0x%08x ", ((char**)(esp+8)) );
     printf("arg1 0x%08x ", (char*)(*((char**)(esp+8))) );
-    printf("arg1 0x%08x ", *(char*)(*((char**)(esp+8))) );
-    printf("arg1 %s ", (char*)(*((char**)(esp+8))) );
+    printf("arg1 0x%08x ", *(char**)(*((char**)(esp+8))) );
+    printf("arg1 %s ", *(char**)(*((char**)(esp+8))) );
     
     if_.esp = (void*) esp;
         
