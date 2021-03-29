c66f3de95db5b32fa113065ea52040cd053d5537
Yang Jiang <yjiang@stackline.com>
2021-03-28 17:22:40 -0700

    arg parse

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -92,7 +92,7 @@ start_process (void *file_name_)
         strsize = sizeof(args[i-1]);
         esp -= strsize;
         printf("size %d", argc);
-        memcpy(esp, args[i-1], strsize);
+        memcpy(esp, &esp, strsize);
     }
     
     /* push char **argv */
