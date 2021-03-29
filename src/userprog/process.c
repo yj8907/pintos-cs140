833feeaf49b0c626a5c34156ff8eeff6519c58aa
Yang Jiang <yjiang@stackline.com>
2021-03-28 18:48:18 -0700

    arg parse

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -70,22 +70,22 @@ start_process (void *file_name_)
   success = load (token, &if_.eip, &if_.esp);
     
   /* extract file arguments */
-    esp = (char*)(&if_.esp);
-    for (argc = 0, argstr = NULL; argc < max_argc;argc++){
-        token = strtok_r(argstr, " ", &saveptr);
-        if (token == NULL) break;
-        strsize = strlen(token) + 1;
-        esp -= strsize;
-        strlcpy(esp, token, strsize);
-        argv[argc] = esp;
-    }
-
-    /* round esp to multiples of 4 */
-    esp -= (uintptr_t)esp % 4;
-
-    /* push null pointer sentinel */
-    esp -= sizeof(esp);
-    memset(esp, 0, sizeof(esp));
+//    esp = (char*)(&if_.esp);
+//    for (argc = 0, argstr = NULL; argc < max_argc;argc++){
+//        token = strtok_r(argstr, " ", &saveptr);
+//        if (token == NULL) break;
+//        strsize = strlen(token) + 1;
+//        esp -= strsize;
+//        strlcpy(esp, token, strsize);
+//        argv[argc] = esp;
+//    }
+//
+//    /* round esp to multiples of 4 */
+//    esp -= (uintptr_t)esp % 4;
+//
+//    /* push null pointer sentinel */
+//    esp -= sizeof(esp);
+//    memset(esp, 0, sizeof(esp));
 
     /* push argv address */
 //    for (int i = argc; i > 0; i--){
