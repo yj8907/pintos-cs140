7834257c5a3b14ae19dd939e1af36a85cf46613a
Yang Jiang <yjiang@stackline.com>
2021-03-28 22:13:04 -0700

    arg parse

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -110,7 +110,7 @@ start_process (void *file_name_)
     memset(esp, 0, sizeof(esp));
     
     printf("arg count %d", *(int*)(esp+4));
-    printf("arg1 %s", *(char*)(*(esp+8)) );
+//    printf("arg1 %s", *(char*)(*(esp+8)) );
     if_.esp = (void*) esp;
         
   /* If load failed, quit. */
