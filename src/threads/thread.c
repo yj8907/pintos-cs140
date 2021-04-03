c31a7bbd7eace123398b79182339a32bd1255268
Yang Jiang <yjiang@stackline.com>
2021-04-02 19:04:18 -0700

    process wait 2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -402,7 +402,7 @@ thread_create (const char *name, int priority,
   /* Initialize thread. */
   init_thread (t, name, priority);
   tid = t->tid = allocate_tid ();
-  t->tcb->tid = tid;
+//  t->tcb->tid = tid;
     
   /* Stack frame for kernel_thread(). */
   kf = alloc_frame (t, sizeof *kf);
