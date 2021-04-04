8f1b4a70efc82c975fd591153f04579c28d20bd4
Yang Jiang <yjiang@stackline.com>
2021-04-03 17:05:08 -0700

    syscall 1

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -53,7 +53,8 @@ syscall_handler (struct intr_frame *f)
     argv += sizeof(fd);
     argv = (char**)argv;
 //    strlcpy(&print_output, *argv, 2);
-    
+    printf("buffer 0x%08x ", argv);
+    printf("buffer 0x%08x ", (char*)(*argv));
     printf("buffer %s ", (char*)(*argv));
     
     argv = (char*)argv;
