4c8b242bcb79858c04b4b752c65dc454c64c3be1
Yang Jiang <yjiang@stackline.com>
2021-04-05 16:44:30 -0700

    thread name

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -156,7 +156,7 @@ sys_exit(void *eax, char** argv)
     int status = *(int*)argv[0];
     
     struct thread *t = thread_current();
-    printf ("%s: exit(%d)\n", t->thread_name(), status);
+    printf ("%s: exit(%d)\n", thread_name(t), status);
     t->tcb->exit_status = status;
     thread_exit();
 };
