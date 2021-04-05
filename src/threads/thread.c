07f9d861bcc91cbaaa5a8176463ea93f6c34e360
Yang Jiang <yjiang@stackline.com>
2021-04-04 22:13:04 -0700

    syscall 3

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -1002,7 +1002,7 @@ fetch_file(int fd_no)
     struct thread *cur = thread_current();
     struct file_descriptor *fd;
     
-    struct list_elem *e = list_front($cur->fildes);
+    struct list_elem *e = list_front(&cur->fildes);
     while (e != list_tail(&cur->fildes)) {
         fd = list_entry(e, struct file_descriptor, elem);
         if (fd->fd_no == fd_no) break;
