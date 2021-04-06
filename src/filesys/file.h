09d799f5b5e0d4f1f2701b243ccf51fcbae8716d
Yang Jiang <yjiang@stackline.com>
2021-04-06 15:47:06 -0700

    debug file close

--- src/filesys/file.h
+++ src/filesys/file.h
@@ -3,14 +3,6 @@
 
 #include "filesys/off_t.h"
 
-/* An open file. */
-struct file
-  {
-    struct inode *inode;        /* File's inode. */
-    off_t pos;                  /* Current position. */
-    bool deny_write;            /* Has file_deny_write() been called? */
-  };
-
 struct inode;
 
 /* Opening and closing files. */
