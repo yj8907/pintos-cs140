bad3639b0a2b77599c65dbf809d67a7df3958e18
Yang Jiang <yjiang@stackline.com>
2021-04-07 01:57:50 -0700

    fix exec arg

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -429,8 +429,7 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
       
-//  if (t->priority > thread_current()->priority)  thread_yield();
-  thread_yield();
+  if (t->priority > thread_current()->priority)  thread_yield();
     
   return tid;
 }
