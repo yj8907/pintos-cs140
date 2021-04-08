9a56bc411c0a7a3271c986804d28fb820761571c
Yang Jiang <yjiang@stackline.com>
2021-04-07 22:00:17 -0700

    fix exec arg

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -211,6 +211,9 @@ static void sys_exec(uint32_t *eax, char** argv)
     const char* cmd_line = *(char**)argv[0];
     validate_char_vaddr(cmd_line);
     
+    printf("ckpt0 exec %s\n", cmd_line);
+    thread_exit();
+    
     tid_t child_tid = process_execute(cmd_line);
     printf("ckpt1 exec %s\n", cmd_line);
     thread_exit();
