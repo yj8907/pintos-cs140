4f5d3c034bdf70beae8f321af11c47b8be729150
Yang Jiang <yjiang@stackline.com>
2021-04-07 22:25:26 -0700

    fix exec arg

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -46,7 +46,7 @@ process_execute (const char *file_name)
   token = strtok_r(file_name, " ", &saveptr);
     
 //        printf("ckpt1 exec process_execute  \n");
-//        thread_exit();
+        thread_exit();
     
   tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
     
