a97b9dd7d86ff2e50034e8e6e5aef1f437f995ee
Yang Jiang <yjiang@stackline.com>
2021-04-08 14:47:39 -0700

    fix exec arg

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -45,14 +45,7 @@ process_execute (const char *file_name)
   char *token, *saveptr;
   token = strtok_r(file_name, " ", &saveptr);
     
-        printf("ckpt1 exec process_execute %s \n", token);
-//        thread_exit();
-//    return TID_ERROR;
   tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
-    printf("thread id %d\n", tid);
-//    return TID_ERROR;
-//    printf("ckpt1 exec process_execute1 %s\n", token);
-//    thread_exit();
     
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
