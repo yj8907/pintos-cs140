cfb6f07b3f34d1fbdd81c21a9b594f5a8c1cd10e
Yang Jiang <yjiang@stackline.com>
2021-04-08 21:26:01 -0700

    exec missing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -74,15 +74,16 @@ start_process (void *file_name_)
   token = strtok_r(file_name, " ", &saveptr);
   
   printf("token %s", token);
-  thread_exit ();
-  success = load (token, &if_.eip, &if_.esp);
+  
+//  success = load (token, &if_.eip, &if_.esp);
       
   /* push arguments */
-  if_.esp = load_argument(if_.esp, token, saveptr);
+//  if_.esp = load_argument(if_.esp, token, saveptr);
   
   thread_current()->tcb->loaded = success;
   sema_up(&thread_current()->tcb->sema);
     
+    thread_exit ();
   /* If load failed, quit. */
   palloc_free_page (file_name);
   if (!success) 
