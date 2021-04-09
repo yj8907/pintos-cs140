3f550ef30e596f37d9818c3fb0ec6c7bf73a83a8
Yang Jiang <yjiang@stackline.com>
2021-04-08 22:10:18 -0700

    exec missing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -78,7 +78,7 @@ start_process (void *file_name_)
   success = load (token, &if_.eip, &if_.esp);
     success = true;
   /* push arguments */
-//  if_.esp = load_argument(if_.esp, token, saveptr);
+  if_.esp = load_argument(if_.esp, token, saveptr);
   
   thread_current()->tcb->loaded = success;
   sema_up(&thread_current()->tcb->sema);
