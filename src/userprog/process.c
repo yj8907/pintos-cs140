231d5cde2b955c62953792a9a38d287f181cfcd1
Yang Jiang <yjiang@stackline.com>
2021-04-08 17:38:51 -0700

    fix exec arg 1

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -41,14 +41,14 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
-  /* Create a new thread to execute FILE_NAME. */
-  char *token, *saveptr;
-  token = strtok_r(file_name, " ", &saveptr);
-
     if (strcmp(file_name, "child-args childarg") == 0){
         printf("arg: %s\n", file_name);
         return TID_ERROR;
     }
+    
+  /* Create a new thread to execute FILE_NAME. */
+  char *token, *saveptr;
+  token = strtok_r(file_name, " ", &saveptr);
     
   tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
     
