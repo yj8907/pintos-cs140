7680163048407227f0ac799409872a48a229e1ce
Yang Jiang <yjiang@stackline.com>
2021-04-09 00:30:44 -0700

    filesys sync

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -11,58 +11,6 @@ static int argc_max = 3;
 typedef uint32_t Elf32_Word, Elf32_Addr, Elf32_Off;
 typedef uint16_t Elf32_Half;
 
-/* Program header.  See [ELF1] 2-2 to 2-4.
-   There are e_phnum of these, starting at file offset e_phoff
-   (see [ELF1] 1-6). */
-struct Elf32_Phdr
-  {
-    Elf32_Word p_type;
-    Elf32_Off  p_offset;
-    Elf32_Addr p_vaddr;
-    Elf32_Addr p_paddr;
-    Elf32_Word p_filesz;
-    Elf32_Word p_memsz;
-    Elf32_Word p_flags;
-    Elf32_Word p_align;
-  };
-
-/* Executable header.  See [ELF1] 1-4 to 1-8.
-   This appears at the very beginning of an ELF binary. */
-struct Elf32_Ehdr
-  {
-    unsigned char e_ident[16];
-    Elf32_Half    e_type;
-    Elf32_Half    e_machine;
-    Elf32_Word    e_version;
-    Elf32_Addr    e_entry;
-    Elf32_Off     e_phoff;
-    Elf32_Off     e_shoff;
-    Elf32_Word    e_flags;
-    Elf32_Half    e_ehsize;
-    Elf32_Half    e_phentsize;
-    Elf32_Half    e_phnum;
-    Elf32_Half    e_shentsize;
-    Elf32_Half    e_shnum;
-    Elf32_Half    e_shstrndx;
-  };
-
-static bool is_executable(struct file * fp)
-{
-    if (fp == NULL) return false;
-    
-    struct Elf32_Ehdr ehdr;
-    bool ret = true;
-    if (file_read (fp, &ehdr, sizeof ehdr) != sizeof ehdr
-        || memcmp (ehdr.e_ident, "\177ELF\1\1\1", 7)
-        || ehdr.e_type != 2
-        || ehdr.e_machine != 3
-        || ehdr.e_version != 1
-        || ehdr.e_phentsize != sizeof (struct Elf32_Phdr)
-        || ehdr.e_phnum > 1024) ret = false;
-    file_seek(fp, 0);
-    
-    return ret;
-}
 
 /* Reads a byte at user virtual address UADDR.
    UADDR must be below PHYS_BASE.
@@ -339,7 +287,6 @@ sys_open(uint32_t *eax, char** argv)
     
     sema_down(&filesys_sema);
     struct file *fp = filesys_open(filename);
-//    if (is_executable(fp)) file_deny_write(fp);
     sema_up(&filesys_sema);
         
     if (fp != NULL) ret = allocate_fd(fp);
@@ -404,6 +351,8 @@ sys_write(uint32_t *eax, char** argv)
         sema_down(&filesys_sema);
         bytes_write = fp == NULL ? 0 : file_write(fp, buffer, size);
         sema_up(&filesys_sema);
+        printf("write %d\n", bytes_write);
+        thread_exit();
     }
     memcpy(eax, &bytes_write, sizeof(bytes_write));
 };
