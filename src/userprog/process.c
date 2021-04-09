ac9e9bce5b9019371e5072a6ea22f1db34cef954
Yang Jiang <yjiang@stackline.com>
2021-04-08 17:48:23 -0700

    fix exec arg 1

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -42,9 +42,11 @@ process_execute (const char *file_name)
   strlcpy (fn_copy, file_name, PGSIZE);
 
     if (strcmp(file_name, "child-args childarg") == 0){
-        printf("arg0: %s\n", file_name);
+        char  *new_filename;
+        strlcpy(new_filename, file_name, strlen(file_name)+1);
+        printf("arg0: %s\n", new_filename);
         char *token, *saveptr;
-        token = strtok_r(file_name, " ", &saveptr);
+        token = strtok_r(new_filename, " ", &saveptr);
         
         printf("arg: %s\n", file_name);
         return TID_ERROR;
