7eec1c4342d41c60a26da179ce3377f397bc06e9
Yang Jiang <yjiang@stackline.com>
2021-04-08 19:28:56 -0700

    fix addr validate

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -80,18 +80,19 @@ validate_vaddr(void *addr, int sz, bool write)
     if ( !is_user_vaddr(addr) || (sz >= 0 && !is_user_vaddr(addr+sz)) ) force_exit();
         
     int count = 0;
-    int byte, success = true;
+    int byte;
+    bool success = true;
     if (sz >= 0){
         while ( count < sz && get_user(addr) != -1){
-            if (write) success = put_user(addr,  0);
-            if (write && !success) break;
+//            if (write) success = put_user(addr,  0);
+//            if (write && !success) break;
             addr++; count++;
         }
     } else {
         while ( is_user_vaddr(addr) && (byte = get_user(addr) != -1)){
             if ( (char)byte == '\0') break;
-            if (write) success = put_user(addr,  0);
-            if (write && !success) break;
+//            if (write) success = put_user(addr,  0);
+//            if (write && !success) break;
             addr++;
         }
     }
