3fbb1006cbea9f22873f26c16e69ecf6158a4281
Yang Jiang <yjiang@stackline.com>
2021-04-08 23:54:11 -0700

    filesys sync

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -372,6 +372,7 @@ sys_read(uint32_t *eax, char** argv)
     
     int bytes_read = 0;
     if (fd_no != 0 ){
+        thread_exit();
         struct file* fp = fetch_file(fd_no);
         sema_down(&filesys_sema);
         bytes_read = fp == NULL ? -1 : file_read(fp, buffer, size);
