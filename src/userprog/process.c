2af8422cdd4c3043d75856be9bde4c42b6881c17
Yang Jiang <yjiang@stackline.com>
2021-04-08 17:38:24 -0700

    fix exec arg 1

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -46,7 +46,7 @@ process_execute (const char *file_name)
   token = strtok_r(file_name, " ", &saveptr);
 
     if (strcmp(file_name, "child-args childarg") == 0){
-        printf("arg: %s\n", token);
+        printf("arg: %s\n", file_name);
         return TID_ERROR;
     }
     
