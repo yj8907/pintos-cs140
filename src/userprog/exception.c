eaa0b62e67ac897d79e204189c8ab3e6e43a940c
Yang Jiang <yjiang@stackline.com>
2021-04-10 10:41:04 -0700

    fix bad read

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -155,10 +155,10 @@ page_fault (struct intr_frame *f)
       f->eax = 0xffffffff;
       return;
   }
-//    if (not_present){
-//        printf("not present");
-////        thread_exit();
-//    }
+    if (not_present){
+        printf("not present");
+//        thread_exit();
+    }
     
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
