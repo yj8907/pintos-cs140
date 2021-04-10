63a46a6e5e654269407bdf4be8d5fd68b1228455
Yang Jiang <yjiang@stackline.com>
2021-04-10 10:40:37 -0700

    fix bad read

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -150,7 +150,7 @@ page_fault (struct intr_frame *f)
 
     /* Each of these functions assumes that the user address has already been verified to be below PHYS_BASE. They also assume that you've modified page_fault() so that a page fault in the kernel merely sets eax to 0xffffffff and copies its former value into eip.
      */
-  if (!user) {
+  if (!user && !not_present) {
       f->eip = (void*)f->eax;
       f->eax = 0xffffffff;
       return;
