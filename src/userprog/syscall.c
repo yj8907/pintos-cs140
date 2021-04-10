db7a135d1db6807a3198677dfd9d70b2522a6e31
Yang Jiang <yjiang@stackline.com>
2021-04-09 17:29:51 -0700

    filesys sync

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -343,7 +343,7 @@ sys_write(uint32_t *eax, char** argv)
     
     validate_vaddr(buffer, size);
     
-    struct file* fp
+    struct file* fp;
     int bytes_write = 0;
     if (fd_no == 1) { // write to stdout
       putbuf(buffer, size);
