1da491828df7178b35bea22043dadd848c20c15f
Yang Jiang <yjiang@stackline.com>
2021-04-10 19:21:20 -0700

    fix palloc

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -152,14 +152,14 @@ page_fault (struct intr_frame *f)
     /* Each of these functions assumes that the user address has already been verified to be below PHYS_BASE. They also assume that you've modified page_fault() so that a page fault in the kernel merely sets eax to 0xffffffff and copies its former value into eip.
      */
     
-  if (!user) {
-      f->eip = (void*)f->eax;
-      f->eax = 0xffffffff;
-      return;
-  }
-    if (user) {
-        force_exit();
-    }
+//  if (!user) {
+//      f->eip = (void*)f->eax;
+//      f->eax = 0xffffffff;
+//      return;
+//  }
+//    if (user) {
+//        force_exit();
+//    }
     
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
