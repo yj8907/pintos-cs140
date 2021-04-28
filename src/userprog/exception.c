7a461b75b9cff6adc4080aa5da6fe6c3ca51e043
Yang Jiang <yjiang@stackline.com>
2021-04-28 16:22:08 -0700

    debug stack growth

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -162,15 +162,15 @@ page_fault (struct intr_frame *f)
   if (not_present) {
 //      void *esp = user ? f->esp : thread_current()->vm_mm->esp;
       
-      void *esp =  f->esp;
-      if (esp != NULL) {
-          if ( (fault_addr >= esp && fault_addr <= PHYS_BASE) ||
-              fault_addr == esp - 4 || fault_addr == esp - 32) {
-              vm_grow_stack(fault_addr);
-              return;
-          }
-      }
-      
+//      void *esp =  f->esp;
+//      if (esp != NULL) {
+//          if ( (fault_addr >= esp && fault_addr <= PHYS_BASE) ||
+//              fault_addr == esp - 4 || fault_addr == esp - 32) {
+//              vm_grow_stack(fault_addr);
+//              return;
+//          }
+//      }
+      force_exit();
 //      page_not_present_handler(fault_addr);
       return;
   }
