1134b82f9a3a5fe4ddf5e3c15ac938e94a23eb14
Yang Jiang <yjiang@stackline.com>
2021-04-28 16:07:48 -0700

    debug stack growth

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -164,7 +164,7 @@ page_fault (struct intr_frame *f)
       
       printf("addr1: 0x%08x\n", f->esp);
       printf("addr1: 0x%08x\n", fault_addr);
-      return;
+      force_exit();
       
       void *esp =  f->esp;
       if (esp != NULL) {
