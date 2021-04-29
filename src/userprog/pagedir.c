1c26c41fad20b69558fd01a2f1adbb2a19eb2a82
Yang Jiang <yjiang@stackline.com>
2021-04-28 22:28:26 -0700

    debug stack growth

--- src/userprog/pagedir.c
+++ src/userprog/pagedir.c
@@ -41,7 +41,7 @@ pagedir_destroy (uint32_t *pd)
         
           for (pte = pt; pte < pt + PGSIZE / sizeof *pte; pte++) {
               if (*pte & PTE_P) {
-                  if (*pte >= PHYS_BASE ) PANIC("bad");
+                  if (*pte >= PHYS_BASE ) PANIC("bad 0x%08x", *pte);
                palloc_free_page (pte_get_page (*pte));
               }
           }
