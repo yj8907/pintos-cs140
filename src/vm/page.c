57aaf11f5756fe8579a0dd68e4023af512fb1114
Yang Jiang <yjiang@stackline.com>
2021-04-28 17:04:31 -0700

    debug vm

--- src/vm/page.c
+++ src/vm/page.c
@@ -167,7 +167,7 @@ page_not_present_handler(void *addr)
     if (va->state == VALID) {
         void *kpage = falloc_get_frame(page, is_user_vaddr(addr) ? PAL_USER | PAL_ZERO : PAL_ZERO);
         
-//        PANIC ("Page fault at 0x%08x\n", addr);
+        PANIC ("Page fault at 0x%08x\n", addr);
         
         
         if (va->data_type != ANONYMOUS) {
