d01755fd5b6af787b8b6ff7200a998512aa60ecd
Yang Jiang <yjiang@stackline.com>
2021-04-28 22:43:09 -0700

    debug stack growth

--- src/userprog/pagedir.c
+++ src/userprog/pagedir.c
@@ -43,7 +43,7 @@ pagedir_destroy (uint32_t *pd)
               if (*pte & PTE_P) {
                   if (*pte >= PHYS_BASE ) PANIC("bad 0x%08x, 0x%08x, 0x%08x, 0x%08x", *pte, pt, pte, pde);
                palloc_free_page (pte_get_page (*pte));
-                  printf("good 0x%08x, 0x%08x, 0x%08x, 0x%08x", *pte, pt, pte, pde);
+                  PANIC("good 0x%08x, 0x%08x, 0x%08x, 0x%08x", *pte, pt, pte, pde);
               }
           }
         palloc_free_page (pt);
