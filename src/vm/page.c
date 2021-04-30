d853f0d1d326a9838f4bd2ad349f96b100bc9f99
Yang Jiang <yjiang@stackline.com>
2021-04-30 10:59:09 -0700

    debug stack growth

--- src/vm/page.c
+++ src/vm/page.c
@@ -176,13 +176,7 @@ page_not_present_handler(void *addr)
     if (va->state == VALID) {
         
         void *kpage = falloc_get_frame(page, is_user_vaddr(addr) ? PAL_USER | PAL_ZERO : PAL_ZERO);
-        if (va->data_type != ANONYMOUS) {
-            if (!load_from_file(va, kpage)) {
-                force_exit();
-            }
-        }
-                
-        va->state = ALLOCATED;
+        
             if (counter == 20) {
                 uint32_t *pt = pde_get_pt (*(thread_current()->pagedir + pd_no(test)));
                 *(pt + pt_no(test)) = *(pt + pt_no(test)) & 0x0;
@@ -196,6 +190,14 @@ page_not_present_handler(void *addr)
                                                      called %d times, this time addr: 0x%08x, is_user: %d, test: 0x%08x, %d pages \n",
                                                  *test, counter, addr, is_user_vaddr(addr), *test, init_ram_pages);
             }
+        
+        if (va->data_type != ANONYMOUS) {
+            if (!load_from_file(va, kpage)) {
+                force_exit();
+            }
+        }
+                
+        va->state = ALLOCATED;
         
         if (!install_page(page, kpage, va->protection == WRITE ? true : false)) force_exit();
     }
