4d586d0083b50e2d0d82c737c4f1270d14511559
Yang Jiang <yjiang@stackline.com>
2021-04-29 23:41:41 -0700

    debug stack growth

--- src/vm/page.c
+++ src/vm/page.c
@@ -175,8 +175,7 @@ page_not_present_handler(void *addr)
     
     if (va->state == VALID) {
         
-//        void *kpage = falloc_get_frame(page, is_user_vaddr(addr) ? PAL_USER | PAL_ZERO : PAL_ZERO);
-        void *kpage = palloc_get_page(PAL_ZERO);
+        void *kpage = falloc_get_frame(page, is_user_vaddr(addr) ? PAL_USER | PAL_ZERO : PAL_ZERO);        
         if (va->data_type != ANONYMOUS) {
             if (!load_from_file(va, kpage)) {
                 force_exit();
@@ -184,7 +183,7 @@ page_not_present_handler(void *addr)
         }
             if (counter == 20) PANIC("bad addr from page_not_present_handler: 0x%08x,\
                                      called %d times, this time addr: 0x%08x, is_user: %d, kpage: 0x%08x, %d pages \n",
-                                 *test, counter, addr, is_user_vaddr(addr), kpage, init_ram_pages);
+                                 *test, counter, addr, is_user_vaddr(addr), thread_current()->pagedir, init_ram_pages);
         va->state = ALLOCATED;
         if (!install_page(page, kpage, va->protection == WRITE ? true : false)) force_exit();
     }
