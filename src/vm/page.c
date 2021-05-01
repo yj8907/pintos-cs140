bc8f0084c054b8240f944a850922fda454163e58
Yang Jiang <yjiang@stackline.com>
2021-05-01 16:18:19 -0700

    release vm

--- src/vm/page.c
+++ src/vm/page.c
@@ -198,7 +198,7 @@ page_not_present_handler(void *addr)
     if (va->state == VALID) {
         
         void *kpage = falloc_get_frame(page, is_user_vaddr(addr) ? PAL_USER | PAL_ZERO : PAL_ZERO);
-//        printf("kpage: 0x%08x, upage:0x%08x \n", kpage, page);
+        printf("kpage: 0x%08x, upage:0x%08x \n", kpage, page);
         if (va->data_type != ANONYMOUS) {
             if (!load_from_file(va, kpage)) {
                 force_exit();
