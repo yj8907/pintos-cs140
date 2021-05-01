c037db8911b20606f51c72847c02d1a2118a81fa
Yang Jiang <yjiang@stackline.com>
2021-04-30 21:12:51 -0700

    debug stack growth

--- src/vm/page.c
+++ src/vm/page.c
@@ -82,8 +82,8 @@ vm_alloc_page(void *page, struct vm_mm_struct* vm_mm, size_t page_cnt,
     
     struct vm_area* vm_area_entry = vm_mm->end_ptr;
     
-    if (pg_round_down(vm_area_entry) == 0xc0113000) PANIC("bad addr from vm_alloc_page: vm_mm->end_ptr addr: 0x%08x, vm_mm addr: 0x%08x, pg_ofs: %d \n",
-                                               vm_mm->end_ptr, vm_mm, pg_ofs(vm_mm->end_ptr));
+    if (pg_round_down(vm_area_entry) == 0xc0113000) PANIC("bad addr from vm_alloc_page: vm_mm->end_ptr addr: 0x%08x, vm_mm addr: 0x%08x, pg_ofs: %d, vm_area size: %d \n",
+                                               vm_mm->end_ptr, vm_mm, pg_ofs(vm_mm->end_ptr), sizeof(struct vm_area));
     
     vm_mm->end_ptr += sizeof(struct vm_area);
     
