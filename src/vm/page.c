cd583045375ac619edad856e7e7049caa56450b9
Yang Jiang <yjiang@stackline.com>
2021-05-01 15:22:05 -0700

    release vm

--- src/vm/page.c
+++ src/vm/page.c
@@ -70,7 +70,7 @@ vm_mm_destroy(struct vm_mm_struct *vm_mm)
         struct vm_area *va = hash_entry (hash_cur (&i), struct vm_area, h_elem);
         void *frame = vm_page_to_frame(thread_current()->pagedir, va->vm_start);
         printf("frame: 0x%08x\n", frame);
-        falloc_free_frame(frame);
+//        falloc_free_frame(frame);
     }
     
 //    PANIC("vm_mm_destroy");
@@ -197,7 +197,7 @@ page_not_present_handler(void *addr)
     if (va->state == VALID) {
         
         void *kpage = falloc_get_frame(page, is_user_vaddr(addr) ? PAL_USER | PAL_ZERO : PAL_ZERO);
-//        printf("kpage: 0x%08x\n", kpage);
+        printf("kpage: 0x%08x, upage:0x%08x \n", kpage, page);
         if (va->data_type != ANONYMOUS) {
             if (!load_from_file(va, kpage)) {
                 force_exit();
