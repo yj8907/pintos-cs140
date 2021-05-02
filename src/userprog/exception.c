312930a9d820e1011086e49debb3431194cf5f8b
Yang Jiang <yjiang@stackline.com>
2021-05-01 18:15:19 -0700

    release vm

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -162,7 +162,7 @@ page_fault (struct intr_frame *f)
   uint32_t stack_sz_limit = 0x04000000;
     
     if (fault_addr < 0x10000000)
-    PANIC("fault addr: 0x%08x\n", fault_addr);
+    PANIC("fault addr: 0x%08x,not_present:%d \n", fault_addr, not_present);
   if (not_present) {
       void *esp = user ? f->esp : thread_current()->vm_mm->esp;
       
