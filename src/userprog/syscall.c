798f37a03df874145f5d9c52257a384bb970d0f8
Yang Jiang <yjiang@stackline.com>
2021-05-01 21:11:46 -0700

    fix sys_write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -97,6 +97,7 @@ validate_vaddr_write(void *addr, uint32_t sz)
     if ( is_user_vaddr(addr) && is_user_vaddr(addr+sz))  {
         for (int i = 0; i < sz; i++) {
             if (!put_user(addr+i, 0)) {
+                PANIC("test");
               if (!put_user(addr+i, 0)) force_exit();
             }
         }
