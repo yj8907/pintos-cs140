62afcadc1ac14f4d1f11cdd2cb2315918066eb60
Yang Jiang <yjiang@stackline.com>
2021-05-06 14:07:43 -0700

    debug mmap close

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -479,7 +479,10 @@ sys_munmap(uint32_t *eax, char** argv)
     int mmap_no = *(int*)argv[0];
     
     struct mmap_descriptor* mmap_d = fetch_mmap(mmap_no);
-    if (mmap_d == NULL) return;
+    if (mmap_d == NULL) {
+        PANIC("test: %d\n", mmap_no);
+        return;
+    }
    
     for (void *pg = mmap_d->start_pg; pg < mmap_d->end_pg; pg += PGSIZE)
         vm_free_page(pg, thread_current()->vm_mm);
