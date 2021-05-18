b922fb4758e2e7d97c0483434479c3d48937eb8a
Yang Jiang <yjiang@stackline.com>
2021-05-18 14:13:16 -0700

    cache read debug 2

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -191,7 +191,7 @@ void
 cache_read(void *cache, void* buffer, size_t offset, size_t size)
 {
     /* read data into memory */
-    printf("cache_read ckpt1\n");
+    printf("cache_read ckpt1, loaded: %d\n", (cache_table + compute_cache_index(cache))->loaded);
     struct cache_entry* e = load_cache(cache);
     memcpy (buffer, cache + offset, size);
     printf("cache_read ckpt2, loaded: %d\n", e->loaded);
