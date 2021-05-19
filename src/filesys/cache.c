effc80f858e0e37702b48d658a6ef8015a0b45c9
Yang Jiang <yjiang@stackline.com>
2021-05-18 17:05:50 -0700

    cache read debug 5

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -130,6 +130,7 @@ load_cache(void *cache)
     struct cache_entry* e = cache_table + compute_cache_index(cache);
     ASSERT(e->sector_no > -1);
     /* read data into memory */
+    printf("loading block0: %d\n", e->sector_no);
     lock_acquire(&e->block_lock);
     printf("loading block1: %d\n", e->sector_no);
     if (!e->loaded) {
