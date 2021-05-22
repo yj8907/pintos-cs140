89008be9b8926ce1da19423485a5b4b59abc4d45
Yang Jiang <yjiang@stackline.com>
2021-05-22 10:16:19 -0700

    sparsefile debug2

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -473,9 +473,9 @@ inode_allow_write (struct inode *inode)
 off_t
 inode_length (const struct inode *inode)
 {
-  off_t length = 0;
+  off_t *length = malloc(4);
   void *cache = cache_allocate_sector(inode->sector, CACHE_READ);
-  cache_read(cache, &length, 0, sizeof(length));
-    PANIC("len: %d\n", *(off_t*)length);
-  return length;
+  cache_read(cache, length, 0, sizeof(length));
+    PANIC("len: %d\n", *length);
+  return *length;
 }
