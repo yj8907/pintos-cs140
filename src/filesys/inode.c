72f8dea394ccd5bd6439bcfc30c264af9a5aeacb
Yang Jiang <yjiang@stackline.com>
2021-05-22 11:36:32 -0700

    sparsefile debug5

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -61,10 +61,8 @@ inode_read_index(block_sector_t block, size_t offset, block_sector_t *sector, bo
     cache_read(cache, sector, offset, ENTRY_SIZE);
     
     if (*sector == 0 && allocate) {
-        block_sector_t sector_tmp = 0;
-//        PANIC("pos1: %d\n", *sector);
-        free_map_allocate (1, &sector_tmp);
-        PANIC("pos: %d\n", sector_tmp);
+        free_map_allocate (1, sector);
+        PANIC("pos: %d\n", *sector);
         cache = cache_allocate_sector(block, CACHE_WRITE);
         block_sector_t sector_read = cache_index_write(cache, sector, offset);
         
