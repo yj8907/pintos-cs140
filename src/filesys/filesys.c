a65c6b0e32ff63fe59d5b9f3ad3d63cddf46569b
Yang Jiang <yjiang@stackline.com>
2021-05-23 23:03:17 -0700

    filesys create fullpath debug4

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -109,13 +109,13 @@ filesys_create (const char *name, off_t initial_size)
   block_sector_t inode_sector = 0;
   struct dir *dir;
   char *filename;
-    
+  bool success = false;
+  
   dir = parse_filepath(name, &filename);
-  if (dir == NULL) PANIC("test:%s\n", filename);
-  bool success = (dir != NULL
-                  && free_map_allocate (1, &inode_sector, true)
+  if (dir != NULL)  success = ( free_map_allocate (1, &inode_sector, true)
                   && inode_create (inode_sector, initial_size, false)
                   && dir_add (dir, filename, inode_sector));
+    
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   
