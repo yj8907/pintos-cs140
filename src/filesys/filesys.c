9a2918c10ac377fb617b4949cc34b2c5834d5bd8
Yang Jiang <yjiang@stackline.com>
2021-05-23 19:58:35 -0700

    filesys create fullpath debug2

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -111,7 +111,7 @@ filesys_create (const char *name, off_t initial_size)
   char *filename;
     
   parse_filepath(name, &dir, &filename);
-       
+  PANIC("name: %s", name);
   bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector, true)
                   && inode_create (inode_sector, initial_size, false)
