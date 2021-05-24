d99dc75c0f8e5bf10d76bdbe18ecbf68c5119200
Yang Jiang <yjiang@stackline.com>
2021-05-23 22:37:25 -0700

    filesys create fullpath debug4

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -70,8 +70,7 @@ parse_filepath(const char *name, char **local_name)
     /* search subdirectories */
     while(filename != NULL && curr_dir != NULL){
       if (!dir_lookup(curr_dir, filename, &dir_inode)) break;
-      
-      PANIC("test:%s\n", name);
+            
       dir_close(curr_dir);
       if (inode_isdir(dir_inode)) {
         curr_dir = dir_open(dir_inode);
@@ -81,6 +80,7 @@ parse_filepath(const char *name, char **local_name)
           curr_dir = NULL;
           break;
       }
+      PANIC("test:%s\n", name);
       filename = strtok_r(NULL, pathsep, &saveptr);
     }
     
