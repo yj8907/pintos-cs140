c4240283cc0d2d1267207af97509519638fe0af2
Yang Jiang <yjiang@stackline.com>
2021-05-26 00:11:43 -0700

    mkdir debug2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -563,7 +563,7 @@ sys_mkdir(uint32_t *eax, char** argv)
     /* empty dir name is not allowed */
     if (strcmp(dirname, "") != 0 && strcmp(dirname, "/") != 0)
         success = filesys_create(dirname, 0);
-    
+    PANIC("test:%d\n", success);
     memcpy(eax, &success, sizeof(success));
     if (!success) return;
     
