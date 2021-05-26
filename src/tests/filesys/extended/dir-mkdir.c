6c1b4b5f966d2c8434daa1610bcede1aed661fa2
Yang Jiang <yjiang@stackline.com>
2021-05-26 15:13:04 -0700

    mkdir debug5

--- src/tests/filesys/extended/dir-mkdir.c
+++ src/tests/filesys/extended/dir-mkdir.c
@@ -10,6 +10,6 @@ test_main (void)
   CHECK (mkdir ("a"), "mkdir \"a\"");
   CHECK (create ("a/b", 512), "create \"a/b\"");
   CHECK (chdir ("a"), "chdir \"a\"");
-//  CHECK (open ("b") > 1, "open \"b\"");
+  CHECK (open ("b") > 1, "open \"b\"");
 }
 
