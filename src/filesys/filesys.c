28a3e2d3fc7d054562722265b859b2e2e97869d0
Yang Jiang <yjiang@stackline.com>
2021-05-26 15:39:36 -0700

    mkdir debug6

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -86,10 +86,10 @@ parse_filepath(const char *name, char **local_name, bool create)
     
     next_filename = strtok_r(NULL, pathsep, &saveptr);
     if (next_filename != NULL || (create && dir_inode != NULL) ||  (!create && dir_inode == NULL)) {
-        dir_close(curr_dir);
-        inode_close(dir_inode);
+        if (curr_dir != NULL) dir_close(curr_dir);
+        if (dir_inode != NULL) inode_close(dir_inode);
+        if (prev_dir != NULL) dir_close(prev_dir);
         curr_dir = NULL;
-        if (create && prev_dir != NULL) dir_close(prev_dir);
         goto done;
     }
         
