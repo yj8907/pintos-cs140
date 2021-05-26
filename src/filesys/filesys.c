567f09769bca45378804dfa152390c5d9b808955
Yang Jiang <yjiang@stackline.com>
2021-05-26 14:55:51 -0700

    mkdir debug5

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -90,6 +90,9 @@ parse_filepath(const char *name, char **local_name, bool create)
         goto done;
     }
     
+    if (strcmp(name, "a")==0) printf("parse_filepath_test:%1\n", dir_inode == NULL);
+    if (strcmp(name, "a")==0) printf("parse_filepath_test:%d\n", dir_inode == NULL);
+    
     if (create) ASSERT(filename != NULL);
     /* to allow to open directory as file */
     if (!create && dir_inode != NULL && prev_dir != curr_dir) { /* file is actually directory */
@@ -97,7 +100,6 @@ parse_filepath(const char *name, char **local_name, bool create)
         curr_dir = prev_dir;
         filename = prev_filename;
     }
-    if (strcmp(name, "a")==0) printf("parse_filepath_test:%d\n", dir_inode == NULL);
     if (filename != NULL) {
         *local_name = malloc(strlen(filename)+1);
         strlcpy(*local_name, filename, strlen(filename)+1);
--- src/tests/filesys/extended/dir-mkdir.c
+++ src/tests/filesys/extended/dir-mkdir.c
@@ -8,7 +8,7 @@ void
 test_main (void) 
 {
   CHECK (mkdir ("a"), "mkdir \"a\"");
-  CHECK (create ("a/b", 512), "create \"a/b\"");
+//  CHECK (create ("a/b", 512), "create \"a/b\"");
   CHECK (chdir ("a"), "chdir \"a\"");
 //  CHECK (open ("b") > 1, "open \"b\"");
 }
