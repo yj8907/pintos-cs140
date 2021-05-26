9070ccf22172ba565d473a8db9cab3fa8bd66e57
Yang Jiang <yjiang@stackline.com>
2021-05-26 00:09:05 -0700

    mkdir debug2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -90,8 +90,7 @@ trim_dir_path(const char* name)
     
     if (strcmp(name, "/") != 0 && strcmp(name+name_size-1, "/") == 0) name_size--;
     dirname = malloc(name_size+1);
-    strlcpy(dirname, name, name_size);
-    dirname[name_size] = '\0';
+    strlcpy(dirname, name, name_size+1);    
     PANIC("test:%s\n", dirname);
     return dirname;
 }
