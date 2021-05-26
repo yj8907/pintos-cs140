52acd0b4f25566a218f87f7dc969242b5c6b9d6f
Yang Jiang <yjiang@stackline.com>
2021-05-25 19:46:55 -0700

    fix filepath parser debug4

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -92,15 +92,14 @@ parse_filepath(const char *name, char **local_name, bool create)
 //    if (curr_dir == NULL) PANIC("test2:%s\n", name);
     if (create) ASSERT(filename != NULL);
     /* to allow to open directory as file */
-    if (prev_dir != curr_dir) {
-        dir_close(curr_dir);
-        curr_dir = prev_dir;
-        filename = prev_filename;
-    }
     if (filename != NULL) {
         *local_name = malloc(strlen(filename)+1);
         strlcpy(*local_name, filename, strlen(filename)+1);
         goto done;
+    }
+    if (prev_dir != curr_dir) {
+        dir_close(curr_dir);
+        curr_dir = prev_dir;        
     }
         
     done:
