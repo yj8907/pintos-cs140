fc72948c09b68725f07b601949660c76a3079c59
Yang Jiang <yjiang@stackline.com>
2021-05-28 00:33:14 -0700

    inode run out of disk space

--- src/tests/filesys/extended/dir-vine.c
+++ src/tests/filesys/extended/dir-vine.c
@@ -79,8 +79,9 @@ test_main (void)
 
       snprintf (file_name, sizeof file_name, "file%d", i);
       snprintf (dir_name, sizeof dir_name, "dir%d", i);
-        PANIC("test: %d\n", i);
+
       CHECK (chdir (".."), "chdir \"..\"");
+        PANIC("test: %d\n", i);
       CHECK (remove (dir_name), "remove \"%s\"", dir_name);
       CHECK (remove (file_name), "remove \"%s\"", file_name);
     }
