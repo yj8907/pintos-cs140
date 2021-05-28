939cc0720418ae5127d1899e427b31714526aee4
Yang Jiang <yjiang@stackline.com>
2021-05-27 23:03:45 -0700

    debug rm cwd2

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -76,8 +76,8 @@ parse_filepath(const char *name, char **local_name, bool create)
     }
     
     filename = strtok_r(fullname, pathsep, &saveptr);
-    if (strcmp(fullname, "/a")==0) printf("test3:%s, %s, %d\n", filename, fullname,  strchr(name, "/")-name);
-    if ( strchr(name, "/")-name == 0) {
+    if (strcmp(fullname, "/a")==0) printf("test3:%s, %s, %d\n", filename, fullname);
+    if ( *name == (int)(*pathsep) ) {
       curr_dir = dir_open_root ();
     }
     else {
