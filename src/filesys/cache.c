160bc67766f87676648079eb4e716a30625a3d65
Yang Jiang <yjiang@stackline.com>
2021-05-29 14:34:39 -0700

    filesys fix inode write/read sync

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -197,7 +197,7 @@ cache_read(void *cache, void* buffer, size_t offset, size_t size)
     e->read_ref--;
     if (e->read_ref == 0) e->state = NOOP;
         
-    if (e->write_ref > 0){
+    if (e->write_ref > 0 && e->read_ref == 0){
         printf("cache_read cond signal write: 0x%08x, thread: %s\n", cache, thread_name());
         cond_signal(&e->write_cv, &e->block_lock);
     }
